1b70ae5cbd32587fb9135ec448d0202099811709
dadario23 <dandrada23@gmail.com>
2025-08-27 14:54:08 -0300

    add configs

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig = {
   reactStrictMode: true,
   images: {
-    domains: ["www.perozzi.com.ar"],
+    domains: ["www.perozzi.com.ar", "www.rodo.com.ar"],
   },
 };
 
